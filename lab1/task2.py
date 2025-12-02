f4e1d0929b07aae57e31cbf31d79b69bc25686f5
lovvvv19 <lakserim253@gmail.com>
2025-12-02 22:39:56 +0500

    Update task2.py

--- lab1/task2.py
+++ lab1/task2.py
@@ -1,1 +1,11 @@
+import math 
 
+x=float(input("Введите значение х: "))
+y=float(input("Введите значение y: "))
+z=float(input("Введите значение z: "))
+
+a=sqrt(x**3/2)-math.sin(y)
+b=(e**2/3)-math.cos(y)+z+math.log(y)
+
+print(f"Получено значение функции a={a}")
+print(f"Получено значение функции b={b}")
